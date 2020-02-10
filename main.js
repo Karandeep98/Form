0b228048b73c08bcab35f11551ec92cb36857a4b
Karandeep Singh <greatprincekarandeep@gmail.com>
2020-02-11 00:45:16 +0530

    bug fixes

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ else if(password.value==""){alert("Password can't be empty");return false;}
 }
 
 function check(){
+  var letters=/^[a-zA-Z]+$/;
   var name=document.getElementById('name')
   var email=document.getElementById('email')
   var mobile=document.getElementById('mobile')
@@ -22,11 +23,16 @@ function check(){
   if(name.value==""){alert("Name can't be empty!!");return false;}
   else if(email.value==""){alert("Email can't be empty!!");return false;}
   else if(mobile.value==""){alert("Mobile Number can't be empty!!");return false;}
+  else if(mobile.value.length!=10){alert("Invalid Mobile Number!!");return false;}
   else if(age.value==""){alert("Age can't be empty!!");return false;}
+  else if(age.value<18){alert("You are not eligible at the moment.Come back after "+ parseInt(18-parseInt(age.value)) +" years!!");return false;}
   else if(city.value==""){alert("City name can't be empty!!");return false;}
+  else if(!city.value.match(letters)){alert("City name can't have numbers or special characters!!");return false;}
   else if(country.value==""){alert("Country name can't be empty!!");return false;}
+  else if(!country.value.match(letters)){alert("Country name can't have numbers or special characters!!");return false;}
   else if(pswd.value==""){alert("Password can't be empty!!");return false;}
   else if(repswd.value==""){alert("Password can't be empty!!");return false;}
+  else if(pswd.value.length<6){alert("Password can't be less than 6 characters!!");return false;}
 else if(pswd.value!=repswd.value){alert("Password doesn't match!!");return false;}
 
 }
